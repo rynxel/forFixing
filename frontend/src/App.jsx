19adbc7001ca9249a99e4bb533ddabb3c13e5f81
Alexander Ivan Ferraz <alexanderivanferraz@gmail.com>
2026-02-18 22:18:59 +0800

    Fixing Frontend display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import api from "./api/axios"
+import { useEffect, useState } from 'react';
 import Tasks from "./Tasks"
-
 function App() {
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    
+    api.get('/api/Users')
+      .then(res => setUsers(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+
   return (
     <div className="app">
       <h1>📝 React Task Evaluator</h1>
-      <Tasks />
+      <Tasks/>
+
     </div>
   );
 }
--- frontend/src/Tasks.jsx
+++ frontend/src/Tasks.jsx
@@ -3,25 +3,32 @@ import api from "./api/axios"
 
 function Tasks() {
   const [tasks, setTasks] = useState([]);
+  const [newTaskTitle, setNewTaskTitle] = useState([]);
+  const [newTask, setNewTask] = useState([]);
 
   useEffect(() => {
     api.get('/tasks')
-      .then(res => setTasks(res.data))
-      .catch(err => console.error(err));
+    .then(res => { setTasks(Array.isArray(res.data) ? res.data : []); })
+    .catch(err => { setTasks([]); });
   }, []);
 
   return (
     <div>
       <h2>Tasks</h2>
       <ul>
-        {tasks.map(task => (
-          <li key={task.id}>
-            {task.title} {task.isDone ? '✅' : '❌'}
-          </li>
-        ))}
+        {tasks && tasks.length > 0 ? (
+          tasks.map(task => (
+            <li key={task.id}>
+              {task.title} {task.isDone ? '✅' : '❌'}
+            </li>
+          ))
+        ) : (
+          <li>No tasks yet</li>
+        )}
       </ul>
     </div>
   );
+  
 }
 
 export default Tasks;
